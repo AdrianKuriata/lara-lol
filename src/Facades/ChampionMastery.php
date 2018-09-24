df889c9890c058cbbb9bd17c93157f291add2112
AdrianKuriata <adriandj83@gmail.com>
2018-09-24 17:38:16 +0200

    Added Champion Mastery facade

---
+++ src/Facades/ChampionMastery.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Devtemple\Laralol\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * ChampionMastery Facade
+ */
+class ChampionMastery extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'champion-mastery';
+    }
+}
