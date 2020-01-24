76d4e3ba91d5322a52fbcac9bf820ad458a8b035
skatecram <20643946+skatecram@users.noreply.github.com>
2020-01-24 20:37:23 +0100

    add api-version to config

--- config/laralol.php
+++ config/laralol.php
@@ -3,5 +3,19 @@
 return [
     'api_key' => env('LARALOL_API_KEY', null),
     'default_server' => 'eun1',
-    'default_season' => 11
+    'default_season' => 10,
+    'api-version' =>[
+        'platform' => [
+            'champion-rotations' => 3,
+            'third-party-code' => 4,
+        ],
+        'champion-mastery' => 4,
+        'league' => 4,
+        'status' => 3,
+        'match' => 4,
+        'spectator' => 4,
+        'summoner' => 4,
+        'tournament' => 4,
+        'tournament-stub' => 4
+    ]
 ];
